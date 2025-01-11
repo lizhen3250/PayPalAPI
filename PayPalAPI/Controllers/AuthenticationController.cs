971cad0d3c4dff07e802c886f2b9e8389e4f55c7
lizhen3250 <zhenli3@paypal.com>
2025-01-11 12:13:29 +0800

    implemented vault access token

--- PayPalAPI/Controllers/AuthenticationController.cs
+++ PayPalAPI/Controllers/AuthenticationController.cs
@@ -9,16 +9,31 @@ namespace PayPalAPI.Controllers;
 [Route("api/[controller]")]
 public class AuthenticationController : ControllerBase
 {
+    private readonly string sandboxEndpoint = "https://api.sandbox.paypal.com";
     //api/authentiation/token
     [HttpPost(template: "token", Name = "token")]
     public async Task<IActionResult> GetAccessTokenAsync()
     {
-        var options = new RestClientOptions("https://api.sandbox.paypal.com");
+        var options = new RestClientOptions(sandboxEndpoint);
         var client = new RestClient(options);
         var request = new RestRequest("/v1/oauth2/token/", Method.Post);
         request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
         request.AddHeader("Authorization", "Basic QWZpOWFZMVE0TlJPdTlxeHVQTmwzcVkzVXJBcWFjRkVyNUEydlFQb01FN3otMHQyeXhLSEZ1SXowRzlqRTJJUWhFbTlXLV9ZeW42M2VPUW46RUlia2RpMXBxT2daOVpPSWFMMFlsNXBWSjZMU0c5X3VNN3RZNjRnZUxpNVRwSXg5QW9HeG44T3Fna3hMSlg4ekw0bmhQMF9TTGFXWW02Uk8=");
         request.AddParameter("grant_type", "client_credentials");
+        RestResponse response = await client.ExecuteAsync(request);
+        return new JsonResult(response.Content);
+    }
+
+    [HttpPost(template: "vaulttoken", Name = "vaulttoken")]
+    public async Task<IActionResult> GetVaultAccessTokenAsync(string customerId)
+    {
+        var options = new RestClientOptions(sandboxEndpoint);
+        var client = new RestClient(options);
+        var request = new RestRequest("/v1/oauth2/token/", Method.Post);
+        request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+        request.AddHeader("Authorization", "Basic QWZpOWFZMVE0TlJPdTlxeHVQTmwzcVkzVXJBcWFjRkVyNUEydlFQb01FN3otMHQyeXhLSEZ1SXowRzlqRTJJUWhFbTlXLV9ZeW42M2VPUW46RUlia2RpMXBxT2daOVpPSWFMMFlsNXBWSjZMU0c5X3VNN3RZNjRnZUxpNVRwSXg5QW9HeG44T3Fna3hMSlg4ekw0bmhQMF9TTGFXWW02Uk8=");
+        request.AddParameter("grant_type", "client_credentials");
+        request.AddParameter("target_customer_id", customerId);
         RestResponse response = await client.ExecuteAsync(request);
         return new JsonResult(response.Content);
     }
--- PayPalAPI/obj/Debug/net8.0/PayPalAPI.AssemblyInfo.cs
+++ PayPalAPI/obj/Debug/net8.0/PayPalAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PayPalAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d630efdb0a53b02485759c84ec571f59b9eac11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64d99208b28deb0a60e2f26381c26aa53462d12a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PayPalAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PayPalAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
